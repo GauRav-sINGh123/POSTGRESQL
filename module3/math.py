353df8d8f373780a7457aa2527eff40ca3386025
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 00:49:42 +0530

    Math Module Basics

---
+++ module3/math.py
@@ -0,0 +1,14 @@
+#Math Module in Python
+
+import math
+
+
+
+x=9.9
+y=16
+
+print(math.floor(x))  # Round downs to Nearest smaller number
+print(math.ceil(x))   # Round ups to Nearest larger number
+print(math.pi)        #Returns the value of pi
+print(math.e)         #Returns the value of e
+print(int(math.sqrt(y)))   #Returns the square root
