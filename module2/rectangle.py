16767cc206af9f02688a3e8aca0d4d3351272c3d
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 00:06:58 +0530

    Find Area And Perimeter Of Rectangle

---
+++ module2/rectangle.py
@@ -0,0 +1,18 @@
+#Area of Rectangle
+print("Area of Rectangle")
+length=float(input("Enter the length of the rectangle:"))
+width=float(input("Enter the width of the rectangle:"))
+
+area=length*width
+
+print(f"The area of the rectangle is {area}")
+
+
+#Perimeter of Rectangle
+print("Perimeter Of Rectangle")
+length=float(input("Enter the length of the rectangle:"))
+width=float(input("Enter the width of the rectangle:"))
+
+perimeter=2*(length+width)
+
+print(f"The perimeter of the rectangle is {perimeter}")
