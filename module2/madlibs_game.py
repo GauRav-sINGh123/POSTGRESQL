d811370700606ec4a0f27f27201fb6748b154421
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 00:31:13 +0530

    MadLib Game

---
+++ module2/madlibs_game.py
@@ -0,0 +1,14 @@
+#Madlibs Game
+#Word Filling Game
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+place1 = input("Enter a place: ")
+
+ 
+print("\nHere's your story:")
+print(f"One day, a {adjective1} {noun1} decided to {verb1} {adverb1} in the {place1}.")
+print(f"Everyone was amazed because no one had ever seen such a {adjective1} {noun1} do that before!")
+print("It became the talk of the town.")
