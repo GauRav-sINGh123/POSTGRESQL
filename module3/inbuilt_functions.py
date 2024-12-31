211ed5e2f1d8eb0f45eece4df68e02e454c34cb6
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 00:44:18 +0530

    Inbuilt Functions In Python

---
+++ module3/inbuilt_functions.py
@@ -0,0 +1,21 @@
+#Inbuilt functions in Python
+
+
+num=2.5
+num2=5.2
+num3=4.3
+
+
+#answer=abs(num) #absolute value
+#answer=pow(num,2) #power of a number
+
+answer=round(num) #Rounds the number to the nearest integer
+
+print(answer)
+
+
+minimum=min(num,num2,num3) #Finds the smallest number
+maximum=max(num,num2,num3) #Finds the largest number
+
+print(minimum)
+print(maximum)
