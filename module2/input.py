dab3b00f18ec7cbcbd2e02c1558538559bd79d88
Gaurav Singh <gksingh947@gmail.com>
2024-12-31 23:59:54 +0530

    User Input

---
+++ module2/input.py
@@ -0,0 +1,9 @@
+name=input("Hey there! What's your name?")
+age=int(input("How old are you?"))
+
+#age=int(age)
+age=age +1
+
+print(f"Hello {name}")
+print ("Happy birthday to you")
+print(f"You are {age} years old")
