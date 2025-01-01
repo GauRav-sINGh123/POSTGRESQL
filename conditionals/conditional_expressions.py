acc3b11134146145c44fb3ac445de734dc5b7981
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 16:09:34 +0530

    Conditional Expressions

---
+++ conditionals/conditional_expressions.py
@@ -0,0 +1,30 @@
+#ShortHand For Evaluation
+
+num=-2
+
+answer="Positive" if num>0 else "Negative"
+print(answer)
+
+
+a=3
+b=5
+
+max=a if a>b else b
+min=a if a<b else b
+print(max)
+print(min)
+
+#Check Temperature
+
+temp=23
+
+print("It's warm" if temp>20 else "It's cold")
+
+#Check User Role
+
+user="customer"
+
+isAdmin="Welcome Admin" if user=="admin" else "Access Denied"
+print(isAdmin)
+
+ 
