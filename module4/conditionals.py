a6774006f1088c7b172e20b3dc84ad531cdd29f4
Gaurav Singh <gksingh947@gmail.com>
2025-01-01 15:16:25 +0530

    Conditionals If Else

---
+++ module4/conditionals.py
@@ -0,0 +1,10 @@
+choice = int(input("What's your favorite activity? \n1: Sleeping \n2: Running \n3: Swimming  "))
+
+if choice == 1:
+    print("You're like a cat: lazy but adorable!")
+elif choice == 2:
+    print("You're like a cheetah: fast and focused!")
+elif choice == 3:
+    print("You're like a dolphin: playful and smart!")
+else:
+    print("You're unique, like a mythical dragon!")
