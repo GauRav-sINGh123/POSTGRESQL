09de9ad6b8007902469a1d32b5221e1b8d109e06
Gaurav Singh <gksingh947@gmail.com>
2025-01-02 17:37:38 +0530

    Set

---
+++ collections/Set.py
@@ -0,0 +1,7 @@
+#Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members but can add and remove
+
+cars={"Volvo", "BMW", "Ford"}
+ 
+cars.add("Honda")
+cars.remove("Volvo")
+print(cars)
